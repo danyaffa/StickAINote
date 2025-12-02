755e209ae63b8cf35e5d589bdc4c95925e44917b
Dan Leffler <leffleryd@gmail.com>
2025-12-02 20:03:23 +1100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
 // FILE: /pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import { AuthProvider } from "../context/AuthContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  // Keep it SIMPLE for now – no extra providers
+  return <Component {...pageProps} />;
 }
