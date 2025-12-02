98e15adaec6087a80a22590a29d61dbfb0c4ded7
Dan Leffler <leffleryd@gmail.com>
2025-12-02 16:17:43 +1100

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+// FILE: pages/_app.tsx
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { AuthProvider } from "../context/AuthContext";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
