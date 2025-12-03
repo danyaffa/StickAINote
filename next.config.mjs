3a6fc1b6684c6b5c76b7f9d73c6760e52fcaa16f
Dan Leffler <leffleryd@gmail.com>
2025-12-03 17:57:24 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,5 +10,5 @@ export default withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Disable in dev to avoid caching issues
+  disable: process.env.NODE_ENV === "development",
 })(nextConfig);
