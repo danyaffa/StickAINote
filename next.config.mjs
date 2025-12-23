868de0535c55da55e80fa1971c1b978831f93098
Dan Leffler <leffleryd@gmail.com>
2025-12-24 08:22:52 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 // next.config.mjs
-import withPWA from "next-pwa";
-
 const nextConfig = {
   reactStrictMode: false,
-  output: "export",
   images: {
     unoptimized: true,
   },
 };
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+export default nextConfig;
